7d34d18e226f4f2141dbfbd8c51c8b063a6d4a1b
user404e <e404user@gmail.com>
2023-06-23 14:06:05 +0000

    JAVA

--- PRACTICAL 24/UserDefineException.java
+++ PRACTICAL 24/UserDefineException.java
@@ -1,5 +1,3 @@
-import java.util.Scanner;
-
 class NotSufficientFund extends Exception {
     public NotSufficientFund(String message) {
         System.out.println(message);
@@ -9,7 +7,7 @@ class NotSufficientFund extends Exception {
 class Bank {
     private int balance = 0;
 
-    int withdraw(int amount) throws NotSufficientFund {
+    int withdraw(int amount) throws NotSufficientFund{
         if (amount > balance) {
             throw new NotSufficientFund("Not Sufficient Funds");
         } else {
@@ -29,10 +27,15 @@ public class UserDefineException {
         Bank b = new Bank();
         System.out.println("Deposited Amount = " + b.deposit(25000));
         try {
-            System.out.println("Widthraw Amount = 20000 | Remaining Amount = " + b.withdraw(20000));
-            System.out.println("Widthraw Amount = 4000  | Remaining Amount = " + b.withdraw(4000));
-            System.out.println("Widthraw Amount = 20000 | Remaining Amount = " + b.withdraw(20000));
+            System.out.print("Widthraw Amount = 20000 | ");
+            System.out.println("Remaining Amount = " + b.withdraw(20000));
+            System.out.print("Widthraw Amount = 4000  | ");
+            System.out.println("Remaining Amount = " + b.withdraw(4000));
+            System.out.println("Widthraw Amount = 2000 ");
+            System.out.println("Remaining Amount = " + b.withdraw(2000));
+            
         } catch (NotSufficientFund e) {
+            
         }
     }
 }
